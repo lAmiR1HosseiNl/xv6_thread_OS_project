defc83fcb9db285a5781237f6fab9c58be49dd30
Ali Shokoohi <94232712+Alibest3@users.noreply.github.com>
2024-01-29 17:25:22 +0330

    thread_create added

--- ulib.c
+++ ulib.c
@@ -3,6 +3,7 @@
 #include "fcntl.h"
 #include "user.h"
 #include "x86.h"
+#include "mmu.h"
 
 char*
 strcpy(char *s, const char *t)
@@ -103,4 +104,12 @@ memmove(void *vdst, const void *vsrc, int n)
   while(n-- > 0)
     *dst++ = *src++;
   return vdst;
+}
+
+int thread_create(void (*start_routine)(void *, void *), void* arg1, void* arg2)
+{
+  void* stack;
+  stack = malloc(PGSIZE);
+
+  return clone(start_routine, arg1, arg2, stack);
 }
