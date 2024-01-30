d8634b0f0ce6eb30d73056998deb4919210ad8eb
lAmiR1HosseiNl <araeghi@gmail.com>
2024-01-30 19:25:02 +0330

    join in proc.c create.

--- proc.c
+++ proc.c
@@ -320,7 +320,10 @@ exit(void)
   sched();
   panic("zombie exit");
 }
-
+int join(void** stack)
+{
+  
+}
 // Wait for a child process to exit and return its pid.
 // Return -1 if this process has no children.
 int
