d53aab8af23db5dd69c311b6c8e5c9104ec1ab4c
lAmiR1HosseiNl <araeghi@gmail.com>
2024-01-30 19:26:43 +0330

    full join in proc.c

--- proc.c
+++ proc.c
@@ -320,10 +320,49 @@ exit(void)
   sched();
   panic("zombie exit");
 }
-int join(void** stack)
+int
+join(void** stack)
 {
-  
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for zombie children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
+      if(p->parent != curproc || p->pgdir != p->parent->pgdir)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        stack = p->threadstack;
+        p->threadstack = 0;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
 }
+
 // Wait for a child process to exit and return its pid.
 // Return -1 if this process has no children.
 int
